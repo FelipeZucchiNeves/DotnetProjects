f8287636b719360dc91cc481edac18fe19850549
Felipe Neves <felipe_neves_2@hotmail.com>
2020-05-16 15:43:38 -0300

    Baby steps TDD

---
+++ TesteDeSoftware/src/NerdStore.Core/DominObjects/DomainException.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace NerdStore.Core.DominObjects
+{
+        public class DomainException : Exception
+    {
+        public DomainException()
+        {  }
+        public DomainException(string message) : base (message)
+        {  }
+        public DomainException(string message, Exception innerException) : base(message, innerException)
+        {  }
+    }
+    
+}
